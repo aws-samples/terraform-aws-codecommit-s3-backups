3ba2b66d841f5273ff3b708a291b7a56386e0914
jakebark <jakebark@amazon.com>
2026-05-14 06:53:24 +0100

    update vars

--- variables.tf
+++ variables.tf
@@ -1,10 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-variable "name" {
-  type = string
-}
-
 variable "access_logging_bucket" {
   description = "s3 server access logging bucket arn"
   type        = string
@@ -12,6 +8,12 @@ variable "access_logging_bucket" {
 }
 
 variable "kms_key" {
-  type    = string
-  default = null
+  description = "ARN of an existing KMS key to encrypt the S3 bucket and CloudWatch log group"
+  type        = string
+  default     = null
+}
+
+variable "name" {
+  description = "Name used for resource naming (CodeBuild project, IAM roles, S3 bucket)"
+  type        = string
 }
