ee537390c11861da2692f3b6b48040ee303098d2
jakebark <jakebark@amazon.com>
2026-05-14 06:50:04 +0100

    added versioning

--- s3.tf
+++ s3.tf
@@ -25,6 +25,13 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.this.id
   policy = data.aws_iam_policy_document.this.json
